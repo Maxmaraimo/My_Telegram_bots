1653b6e9c0207cbb071496d6f86cc484736b77f1
Ozodbek <ozodbekmahmaraimov16@gmail.com>
2023-08-16 12:03:51 -0900

    first

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ API = '4ed218357d9c16888596f3b54e587aac'
 
 
 
-
-
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Here are the available commands:',
+                     parse_mode='markdown')
+    bot.send_message(message.chat.id, '/start - start the bot')
+    bot.send_message(message.chat.id, '/help - get help')
+    bot.send_message(message.chat.id, '/searchjop - search for a job online')
+    bot.send_message(message.chat.id, '/photo - send a photo')
+    bot.send_message(message.chat.id, '/weather - get the weather forecast')
+    bot.send_message(message.chat.id, '/sum - calculate the sum of two numbers')
 
 
 
@@ -33,83 +41,44 @@ def site(message):
     webbrowser.open('https://itproger.com')
 
 
-@bot.message_handler(commands=['start','main','hello'])
-def main(message):
-    bot.send_message(message.chat.id, f'Hello!, MR. {message.from_user.first_name} write /help')
+
+
+@bot.message_handler(commands=['start', 'main', 'hello'])
+def welcome(message):
+    bot.send_message(message.chat.id, f'Welcome, {message.from_user.first_name}! Type /help to see the available commands.')
 
 
 
-@bot.message_handler(commands=['searchjop'])
-def main(messages):
-    bot.send_message(messages.chat.id, '<b>Search</b> <em><u>jop</u></em>', parse_mode='html')
+@bot.message_handler(commands=['searchjob'])
+def search_job(message):
+    bot.send_message(message.chat.id, 'Let me help you find a job!')
     markup = types.InlineKeyboardMarkup()
-    btn11 = types.InlineKeyboardButton('Search for a jop online', url='https://www.upwork.com')
-    btn22 = types.InlineKeyboardButton('Programming lessons', url='https://itproger.com')
-    # btn33 = types.InlineKeyboardButton('What is happened UPWORK', callback_data='saa')
-    markup.row(btn11, btn22)
-    bot.reply_to(messages,  '<b>Search</b> <em><u>jop</u></em>', parse_mode='html', reply_markup=markup)
+    btn1 = types.InlineKeyboardButton('Search for a job online', url='https://www.upwork.com')
+    btn2 = types.InlineKeyboardButton('Programming lessons', url='https://itproger.com')
+    markup.row(btn1, btn2)
+    bot.send_message(message.chat.id, 'What kind of job are you looking for?', reply_markup=markup)
 
-
-
-@bot.message_handler(commands=['help'])
-def main(messages):
-    bot.send_message(messages.chat.id, """
-/start   - Start the bot
-/help    - Help
-/searchjop - he can search jop online
-/photo - you send photo he said
-/weather - search weather 
-/sum - summa
-id - your ID 
-                     
-""")
-
-
-
-@bot.message_handler()
-def info(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, f'Hello!, {message.from_user.first_name} ')
-    elif message.text.lower() == 'id':
-        bot.reply_to(message, f'ID {message.from_user.id}')
-
-
-
-
-currency = 1
-amount = 0
 
 @bot.message_handler(commands=['sum'])
 def sum(message):
-    bot.send_message(message.chat.id, 'Привет, введите сумму')
-    bot.register_next_step_handler(message, summa)
+    bot.send_message(message.chat.id, 'Enter the first number:', parse_mode='markdown')
+    bot.register_next_step_handler(message, get_first_number)
 
-def summa(message):
-    global amount
+def get_first_number(message):
     try:
-        amount = int(message.text.strip())
+        first_number = int(message.text)
+        bot.send_message(message.chat.id, 'Enter the second number:', parse_mode='markdown')
+        bot.register_next_step_handler(message, calculate_sum, first_number)
     except ValueError:
-        bot.send_message(message.chat.id, 'Неверный формат. Введите сумму')
-        bot.register_next_step_handler(message, summa)
-        return
+        bot.send_message(message.chat.id, 'Invalid input. Please enter a valid number.')
 
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    btn99 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
-    btn88 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
-    btn77 = types.InlineKeyboardButton('USD/GBP', callback_data='usd/gbp')
-    btn66 = types.InlineKeyboardButton('Другое значение', callback_data='else')
-
-    markup.add(btn99, btn88, btn77, btn66)
-    bot.send_message(message.chat.id, 'Выберите пару валют', reply_markup=markup)
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    values = call.data.upper().split('/')
-    res = currency.convert(amount, values[0], values[1])
-    bot.send_message(call.message.chat.id, f'Получается: {res}. Можете писать снова.')
-    bot.register_next_step_handler(call.message, summa)
-
-
+def calculate_sum(message, first_number):
+    try:
+        second_number = int(message.text)
+        total = first_number + second_number
+        bot.send_message(message.chat.id, f'The sum is {total}')
+    except ValueError:
+        bot.send_message(message.chat.id, 'Invalid input. Please enter a valid number.')
 
 
 
@@ -119,26 +88,31 @@ def weather(message):
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
-    city = message.text.strip().lower()
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-    data = res.json()
-    temperature = data['main']['temp']
-    bot.reply_to(message, f'Сейчас погода: {temperature}°C')
+    try:
+        city = message.text.strip().lower()
+        res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
 
-
-
-bot.polling()
-
-
-
-
-
-
-
+        if res.status_code == 200:
+            data = res.json()
+            temperature = data['main']['temp']
+            bot.reply_to(message, f'Сейчас погода: {temperature}°C')
+        else:
+            bot.reply_to(message, 'Не удалось получить данные о погоде')
+    except Exception as e:
+        bot.reply_to(message, 'Произошла ошибка при получении данных о погоде')
 
 
 
 
+
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Welcome!, {message.from_user.first_name} ')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID {message.from_user.id}')
 
 
 
@@ -150,13 +124,6 @@ bot.polling()
 def callback_message(callback):
     if callback.data == 'delete':
         bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
-
-
-
-
-
-
-
 
 
 bot.polling(none_stop=True)
